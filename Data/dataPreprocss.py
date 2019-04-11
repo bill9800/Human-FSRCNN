28313a22fade3f1027e3f1893a39ebbab173aec2
bill9800 <bill980000@gmail.com>
2019-04-10 20:53:20 -0700

    add crop function to maintain factor size

--- Data/dataPreprocss.py
+++ Data/dataPreprocss.py
@@ -56,12 +56,42 @@ def create_database(original_dir,store_dir,down_factor):
         cv2.imwrite(store_path,img)
 
 
+def crop_with_scale(original_dir,store_dir,size_factor):
+    # ensure the data is factors of size_factor
+    init_dir(store_dir)
+    size = len(os.listdir(original_dir))
+    for i in range(size):
+        path = original_dir + '/' + str(i) + '.jpg'
+        img = cv2.imread(path)
+        height, width, channels = img.shape
+        new_h = int(height/size_factor)*size_factor
+        new_w = int(width/size_factor)*size_factor
+        img = img[:new_h,:new_w,:]
+        store_path = store_dir + "/" + str(i) + ".jpg"
+        cv2.imwrite(store_path, img)
+
+
+def data_augment(img_dir):
+    detector = MTCNN()
+    imgs = os.listdir(img_dir)
+    for name in imgs:
+        img = cv2.imread(img_dir+'/'+name)
+        faces = detector.detect_faces(img)
+        for face in faces:
+            print(img.shape)
+            print(face)
+            box = face['box']
+            cv2.rectangle(img,(box[0],box[1]),(box[0]+box[3],box[1]+box[2]),(255,0,0),2)
+            img = cv2.resize(img,(int(img.shape[0]/4),int(img.shape[1]/4)))
+            cv2.imshow('image',img)
+            k = cv2.waitKey(0)
+
 
 if __name__ == "__main__":
     #two_K_human_img_selection("original_img","input_img2",0)
-    create_database(ORIGINAL_PATH,STORE_PATH,DOWN_SCALE_FACTOR)
-
-
+    #crop_with_scale('HR_img','HR_img_4',4)
+    create_database('HR_img_4','LR_img_0.25',0.25)
+    #data_augment('HR_img')
 
 
 
