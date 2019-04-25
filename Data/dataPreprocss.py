47c1fddd2f0e4f4c8baeee22b83e04e9f6b6b9d1
bill9800 <bill980000@gmail.com>
2019-04-24 22:07:07 -0700

    add face crop function

--- Data/dataPreprocss.py
+++ Data/dataPreprocss.py
@@ -72,26 +72,61 @@ def crop_with_scale(original_dir,store_dir,size_factor):
 
 
 def data_augment(img_dir):
+    # temp function, need to modify
+    detector = MTCNN()
+    imgs = os.listdir(img_dir)
+    for name in imgs:
+        img = cv2.imread('HR_img/244.jpg')
+        #img = cv2.imread(img_dir+'/'+name)
+        faces = detector.detect_faces(img)
+        print('detected len;',len(faces))
+        for face in faces:
+            print(img.shape)
+            print(face)
+            box = face['box']
+            cv2.rectangle(img,(box[0],box[1]),(box[0]+box[2],box[1]+box[3]),(255,0,0),2)
+            img = cv2.resize(img,(int(img.shape[0]/4),int(img.shape[1]/4)))
+            cv2.imshow('image',img)
+            k = cv2.waitKey(0)
+
+def face_crop(img_dir,store_dir):
+    init_dir(store_dir)
     detector = MTCNN()
     imgs = os.listdir(img_dir)
     for name in imgs:
         img = cv2.imread(img_dir+'/'+name)
         faces = detector.detect_faces(img)
-        for face in faces:
-            print(img.shape)
+        print('detect img - ' + name)
+        print('detected len:',len(faces))
+        for i in range(len(faces)):
+            face = faces[i]
             print(face)
             box = face['box']
-            cv2.rectangle(img,(box[0],box[1]),(box[0]+box[3],box[1]+box[2]),(255,0,0),2)
-            img = cv2.resize(img,(int(img.shape[0]/4),int(img.shape[1]/4)))
-            cv2.imshow('image',img)
-            k = cv2.waitKey(0)
+            confidence = face['confidence']
+            if confidence < 0.95:
+                # threshold to get just high confidence image
+                continue
+            skip = False
+            for par in box:
+                # box is out of range
+                if par < 0 :
+                    skip = True
+            if skip:
+                continue
+            crop_img = img[box[1]:box[1]+box[3],box[0]:box[0]+box[2]]
+            prefix = name.split('.')[0]
+            store_path = store_dir + "/" + prefix + '_' + str(i) + ".jpg"
+            cv2.imwrite(store_path, crop_img)
+
+
 
 
 if __name__ == "__main__":
     #two_K_human_img_selection("original_img","input_img2",0)
     #crop_with_scale('HR_img','HR_img_4',4)
-    create_database('HR_img_4','LR_img_0.25',0.25)
+    #create_database('HR_img_4','LR_img_0.25',0.25)
     #data_augment('HR_img')
+    face_crop('HR_img','face_img')
 
 
 
